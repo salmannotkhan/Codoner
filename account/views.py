675f6be26f1537968a0f80ccbc129bf4e44958f1
Salman Shaikh <tony903212@gmail.com>
2021-06-20 12:16:19 +0530

    Added Question and TestCase models

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, logout, login
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from main.models import Question, TestCase
 
 
 def login_view(request):
@@ -35,4 +36,28 @@ def dashboard_view(request):
 
 
 def add_view(request):
+    if request.method == "POST":
+        question_title = request.POST.get('question_title')
+        question_desc = request.POST.get('question_desc')
+
+        test1_input = request.POST.get('test1_input')
+        test1_output = request.POST.get('test1_output')
+        test2_input = request.POST.get('test2_input')
+        test2_output = request.POST.get('test2_output')
+        test3_input = request.POST.get('test3_input')
+        test3_output = request.POST.get('test3_output')
+
+        question = Question(title=question_title, description=question_desc)
+        question.save()
+
+        testcase = TestCase(input=test1_input,
+                            output=test1_output, question=question)
+        testcase.save()
+        testcase = TestCase(input=test2_input,
+                            output=test2_output, question=question)
+        testcase.save()
+        testcase = TestCase(input=test3_input,
+                            output=test3_output, question=question)
+        testcase.save()
+
     return render(request, 'add_question.html')
--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    # competition = models.ForeignKey(Competition, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class TestCase(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    input = models.CharField(max_length=256)
+    output = models.TextField()
+
+    def __str__(self):
+        return f'{self.question}({self.input})'
